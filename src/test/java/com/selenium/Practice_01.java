4c2647c77c8f01198bbcdc774e47939e6066111d
AshyrB <berdinyaz@gmail.com>
2022-11-11 09:00:12 -0500

    Practice_01 2nd commit

--- src/test/java/com/selenium/Practice_01.java
+++ src/test/java/com/selenium/Practice_01.java
@@ -1,6 +1,8 @@
 package com.selenium;
 
+import dev.failsafe.internal.util.Assert;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -15,7 +17,12 @@ public class Practice_01 {
         WebDriver driver = new ChromeDriver();
         driver.get("https://news.sportbox.ru");
         driver.manage().window().maximize();
-        driver.findElement(By.xpath("(//a/span[.=\"Хоккей\"])[1]")).click();
+        //driver.findElement(By.xpath("(//a/span[.=\"Хоккей\"])[1]")).click();
+        String actualTitle = driver.getTitle();
+        System.out.println("actualTitle = " + actualTitle);
+        Assertions.assertTrue(actualTitle.contains("Новости"));
+
+        driver.close();
 
 
     }
